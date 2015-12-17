cbe9f02499e826cce0f67b88ff195fe66ae12e6a
andela-gjames <george.james@andela.com>
2015-12-18 00:24:57 +0100

    (Feature) Added Insert capability to SQL Class
    Added the ability to perform insert queries using the StaticSQLQuery class
    utility method to_array also was created to help converting objects to array

--- src/System/StaticSQLQuery.php
+++ src/System/StaticSQLQuery.php
@@ -54,4 +54,52 @@ class StaticSQLQuery
         }
         return $result;
     }
+
+    /**
+     * inserts new data into database table
+     * @param  mixed [$data         = null] object of class to insert
+     * @return boolean  of result of query
+     */
+    public static function insert($data = null)
+    {
+        //Through Exception if $data is null
+        if ($data === null) {
+            throw new \InvalidArgumentException("Cannot save null to the database table ".self::$tableName);
+        }
+
+        //Use util method to convert data to array
+        $data = self::to_array($data);
+
+        //Get the columns/fields and convert them to string
+        $fields     = implode(", ", array_keys($data));
+
+        //Create placeholders based on the number of data to insert
+        $bindable   = implode(", ", array_values(array_fill(0, count($data), '?')));
+
+        //Get the array of data to insert into database
+        $values     = array_values($data);
+
+        //Build the query
+        $query = "INSERT INTO ".self::$tableName." ($fields) VALUES ($bindable) ";
+
+        try {
+            //Prepare the query
+            $statement = static::$dbHandler->prepare($query);
+            //Excecute and return boolean
+            return $statement->execute($values);
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+
+    /**
+     * Converts object to array by casting
+     * @param  mixed $object class object to be converted
+     * @return array of converted object
+     */
+    private static function to_array($object)
+    {
+        return (array)$object;
+    }
+
 }
