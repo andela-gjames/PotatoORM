d2d64abb554dee8453a85e876e4d958a71e4b91e
andela-gjames <george.james@andela.com>
2015-12-21 17:48:15 +0100

    (Feature) Added test file
    Test file to test the functionality of a particular ORM model
    functionalities tested include
    1. 	Getting all properties in model
    2.	Finding a particular record
    3. 	updating a particular record
    4.	creating a new record
    5.	Deleting a record and
    6.	finding a record based on passed paramters

---
+++ tests/UserModelTest.php
@@ -0,0 +1,72 @@
+<?php
+namespace BB8\Potatoes\Tests;
+
+use BB8\Tests\Mocks\User;
+
+class UserStubTest extends \PHPUnit_Framework_TestCase
+{
+    protected $user;
+
+    public function setUp()
+    {
+        User::setUpDB();
+    }
+
+    /**
+     * Test to ensure all returned values are correct
+     */
+    public function testGetAll()
+    {
+        $allUsers = User::getAll();
+
+        $this->assertInternalType('array', $allUsers);
+        //Assert correct property values returned
+        $this->assertSame('Hedy Copeland', $allUsers[0]->full_name);
+        $this->assertSame('faucibus ut, nulla. Cras eu tellus eu augue', $allUsers[1]->description);
+        $this->assertSame('3246', $allUsers[1]->token);
+    }
+
+    public function testFind()
+    {
+        $user = User::find(1);
+
+        //assert that returned type is correct
+        $this->assertInstanceOf("BB8\Tests\Mocks\User", $user);
+        $this->assertSame("1", $user->id);
+        $this->assertSame('Hedy Copeland', $user->full_name);
+    }
+
+    public function testSelectWhere()
+    {
+        $users = User::selectWhere(array("full_name"=>"Hedy Copeland"));
+        $this->assertInternalType('array', $users);
+        $this->assertSame('Hedy Copeland', $users[0]->full_name);
+    }
+
+    public function testCreate()
+    {
+        $user = new User();
+        $user->full_name = "James George Okpe";
+        $user->description = "There is no knowledge that is not power";
+        $user->save();
+
+        $count = count($user::getAll());
+        $this->assertSame(4, $count);
+    }
+
+    public function testUpdate()
+    {
+        $user = User::find(2);
+        $user->full_name = "Darth Vader";
+        $user->save();
+        $updatedUser = User::find(2);
+        $this->assertSame("Darth Vader", $updatedUser->full_name);
+    }
+
+    public function testDelete()
+    {
+        $user = User::find(1);
+        User::destroy($user->id);
+        $this->assertSame(null, User::find(1));
+    }
+}
